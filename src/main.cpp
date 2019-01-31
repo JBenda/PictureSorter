c836dc4b9362b7e3b7fc43fdec038ff06741274b
JBenda <Zabuza98@gmx.net>
2019-01-31 20:29:59 +0100

    Trasnfare

--- src/main.cpp
+++ src/main.cpp
@@ -20,11 +20,9 @@ int main(void) {
 		B[i] = LIST_SIZE - i;
 	}
 
-	// Load the kernel source code into the array source_str
 	FILE *fp;
 	char *source_str;
 	size_t source_size;
-
 	fp = fopen("vector_add.cl", "r");
 	if (!fp) {
 		fprintf(stderr, "Failed to load kernel.\n");
@@ -34,7 +32,7 @@ int main(void) {
 	source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
 	fclose(fp);
 
-	// Get platform and device information
+	
 	cl_platform_id platform_id = NULL;
 	cl_device_id device_id = NULL;
 	cl_uint ret_num_devices;
@@ -43,13 +41,10 @@ int main(void) {
 	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1,
 		&device_id, &ret_num_devices);
 
-	// Create an OpenCL context
 	cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
 
-	// Create a command queue
 	cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
 
-	// Create memory buffers on the device for each vector 
 	cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
 		LIST_SIZE * sizeof(int), NULL, &ret);
 	cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
@@ -57,23 +52,17 @@ int main(void) {
 	cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
 		LIST_SIZE * sizeof(int), NULL, &ret);
 
-	// Copy the lists A and B to their respective memory buffers
 	ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
 		LIST_SIZE * sizeof(int), A, 0, NULL, NULL);
 	ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0,
 		LIST_SIZE * sizeof(int), B, 0, NULL, NULL);
 
-	// Create a program from the kernel source
 	cl_program program = clCreateProgramWithSource(context, 1,
 		(const char **)&source_str, (const size_t *)&source_size, &ret);
 
-	// Build the program
 	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
-
-	// Create the OpenCL kernel
 	cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
 
-	// Set the arguments of the kernel
 	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
 	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
 	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
