149bca4f5cf0bb6a15a8e0f879ad5efebbdbb347
= <=>
2019-12-01 20:26:01 +0100

    added picture test

--- src/fJPG/test/testDecode.cpp
+++ src/fJPG/test/testDecode.cpp
@@ -8,21 +8,38 @@
 #define SAMPLE_DIR "."
 #endif // !SAMPLE_DIR
 
+bool compare(const std::string& p1, const std::string& p2) {
+	std::ifstream
+		f1(p1, std::ios::binary | std::ios::ate),
+		f2(p2, std::ios::binary | std::ios::ate);
+	if (!(f1 && f2) || !(f1.tellg() == f2.tellg())) { return false; }
+	f1.seekg(0, std::ios::beg);
+	f2.seekg(0, std::ios::beg);
+
+	return std::equal(
+		std::istreambuf_iterator<char>(f1.rdbuf()),
+		std::istreambuf_iterator<char>(),
+		std::istreambuf_iterator<char>(f2.rdbuf())
+	);
+
+}
 
 int main(int argc, char* argv[]) {
 	{
-		std::ifstream img(std::string(SAMPLE_DIR) + "/t1.jpeg", std::ios::binary);
+		std::string base(SAMPLE_DIR);
+		std::ifstream img(base + "/t1.jpeg", std::ios::binary);
 		try {
 			fJPG::Picture picture = fJPG::Convert(img);
+			img.close();
 			decltype(picture) const& pic = picture;
-			std::size_t x = 0;
+			std::ofstream out(base + "/t1.pgm", std::ios::binary);
+			out << "P5\n" << pic.GetSize().x << ' ' << pic.GetSize().y << "\n255\n";
 			for (auto c : pic.GetChannel(0)) {
-				std::cout << (int)c << '\t';
-				if (++x == pic.GetSize().x) {
-					std::cout << '\n';
-					x = 0;
-				}
+				out << c;
 			}
+			out.close();
+			assert(compare(base + "/t1.pgm", base + "/t1.pgm.res"));
+
 		}
 		catch (const std::string & msg) {
 			std::cerr << "conversion failed with: " << msg << '\n';
