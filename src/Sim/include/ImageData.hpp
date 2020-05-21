abc3e594658520efedfb40e39e06212409b50a77
Julian Benda <julian.benda@ovgu.de>
2020-05-22 00:51:54 +0200

    Added print, and exclude PHash
    added ImageData print (toString) Method
    exclude PHash in it's own class

--- src/Sim/include/ImageData.hpp
+++ src/Sim/include/ImageData.hpp
@@ -3,10 +3,25 @@
 #include "Settings.hpp"
 #include "md5.h"
 
-#include <iosfwd>
 #include <filesystem>
-#include <vector>
 #include <fstream>
+#include <iosfwd>
+#include <sstream>
+#include <string>
+#include <vector>
+
+
+class PHash {
+public:
+  PHash(const color_t* Y, std::size_t w, std::size_t h);
+  PHash() {}
+  int hammingDistance(const PHash& hash) const; 
+  friend std::ostream& operator<<(std::ostream& os, const PHash& hash);
+private:
+    // hamming trash-hold = 22
+    static constexpr std::size_t s_array_size =
+      (8+sizeof(unsigned int)-1)/ sizeof(unsigned int);
+	std::array<unsigned int, s_array_size> _phash;};
 
 class ImageData
 {
@@ -16,7 +31,7 @@ public:
 			const color_t* Y,
 			const std::size_t w,
 			const std::size_t h)
-		: path{path} 
+		: _path{path},_phash(Y,w,h) 
 	{
 		if ( !std::filesystem::exists(path) ) {
 			std::runtime_error("file: '" + path.string() + "' don't exist");
@@ -24,19 +39,25 @@ public:
 
 		calculateMd5Hash(path);
 
-		calculatePHash(Y, w, h);
-
 	}
 	ImageData() {}
+    std::string print() const {
+      std::stringstream ss;
+      char md5[33];
+      md5_sig_to_string(_md5Hash.data(), md5, 33);
+      ss << "Path: " << _path
+        << "\nMD5: " << md5
+        << "\nphash: " << _phash;
+      return ss.str();
+    }
 	friend std::ostream& operator<<(std::ostream& os, const ImageData& img);
 	friend std::istream& operator>>(std::istream& is,  ImageData& img);
 	std::size_t size() const { return 0; /*TODO: add function*/ }
-
+    const PHash& phash() const { return _phash; }
 private:
 	void calculateMd5Hash(const std::filesystem::path& path);
-	void calculatePHash(const color_t* Y, std::size_t w, std::size_t h);
-	std::filesystem::path path;
+	std::filesystem::path _path;
 	std::array<int, (16 + sizeof(int) - 1) / sizeof(int)> _md5Hash;
-	std::array<int,(8 + sizeof(int) - 1) / sizeof(int)> _phash; // hamming trash-hold = 22
+    PHash _phash;
 };
 
--- src/Sim/src/ImageData.cpp
+++ src/Sim/src/ImageData.cpp
@@ -2,6 +2,8 @@
 
 #define _USE_MATH_DEFINES
 #include <cmath>
+#include <bit>
+#include <algorithm>
 
 void ImageData::calculateMd5Hash(const std::filesystem::path& path) {
 		std::ifstream file(path, std::ios::binary);
@@ -25,16 +27,16 @@ float constexpr sqrtNewtonRaphson(float x, float curr, float prev) {
 constexpr float const_sqrt(float x) { return sqrtNewtonRaphson(x, x, 0); }
 
 float dct_matrix(int x, int y) { // TODO: make constexpr 
-	const float c1 = const_sqrt(2.f / 32.f);
-	return c1 * cos((M_PI / 2.f / 32.f) * y * (2 * x + 1));
+	const float c1 = std::sqrt(2.f/32.f); // const_sqrt(2.f / 32.f);
+	return c1 * cos((M_PI / 2.f / 32.f) * static_cast<float>(y * (2 * x + 1)));
 }
 
 float dct_t_matrix(int x, int y) { // TODO: make constexpr
-	return dct_matrix(31 - x, 31 - y);
+	return dct_matrix(y,x);
 }
 
 // based on phash.org dct hash
-void ImageData::calculatePHash(const color_t* Y, std::size_t w, std::size_t h) {
+PHash::PHash(const color_t* Y, std::size_t w, std::size_t h) {
 	// mean filter 7 x 7
 	// resize 32 x 32 nearest neighbor
 	// dct_matrix (m) 32x32 1 / sqrt(32) 
@@ -52,16 +54,17 @@ void ImageData::calculatePHash(const color_t* Y, std::size_t w, std::size_t h) {
 	
 	for (int x = 0; x < 32; ++x) {
 		for (int y = 0; y < 32; ++y) {
-				int cx = x * wRatio;
+				/*int cx = x * wRatio;
 				int cy = y * hRatio;
-				float& p = img[cx + cy * 32];
+				float& p = img[x + y * 32];
 				int offset = cx + cy * w;
 				for (int xs = 0; xs < 7; ++xs) {
 					for (int xy = 0; xy < 7; ++xy) {
 						p += Y[offset + xs + xy * w];
 					}
 				}
-				p /= 49.f;
+				p /= 49.f; */
+          img[x + y * 32] = Y[static_cast<int>(x * wRatio + y * hRatio * w)];
 		}
 	}
 
@@ -86,25 +89,48 @@ void ImageData::calculatePHash(const color_t* Y, std::size_t w, std::size_t h) {
 
 	std::array<float, 64> streak;
 	std::size_t px = 0;
-	float mean = 0;
 	for (int x = 1; x < 9; ++x) {
 		for (int y = 1; y < 9; ++y) {
-			float& p = img[x + y * 32];
-			mean += p;
-			streak[px++] = p;
+			streak[px++] = img[x + y * 32];
 		}
 	}
-	mean /= 64.f;
+    float median;
+    {
+      std::array<float, 64> list = streak;
+      std::sort(list.begin(),list.end());
+      median = list[32];
+    }
 	for (int i = 0; i < 64;) {
 		for(auto j = 0; j < _phash.size(); ++j) {
 			_phash[j] = 0;
-			for( int k = 0; k < sizeof(decltype(_phash)::value_type); ++k, ++i) {
-				if (streak[i] > mean) {
+			for( int k = 0; k < 8*sizeof(decltype(_phash)::value_type); ++k, ++i) {
+				if (streak[i] > median) {
 					_phash[j] |= (0x01 << k); 
 				}
 			}
 		}
 	}
+}
+
+int PHash::hammingDistance(const PHash& hash) const {
+  auto litr = hash._phash.cbegin(), 
+       ritr = _phash.cbegin();
+  int diff = 0;
+  while(ritr != _phash.cend()) {
+    diff +=  std::__popcount<unsigned int>((*litr)^(*ritr));
+    ++litr; ++ritr;
+  }
+  return diff;
+}
+
+std::ostream& operator<<(std::ostream& os, const PHash& hash) {
+  constexpr int len = sizeof(hash._phash[0]) * 8;
+  auto itr = hash._phash.cbegin();
+  for(int i = 0; i < 64; ++i) {
+    os << (0 != (*itr & (0x1 << (i % len))));
+    if (i%len == len-1) { ++itr; }
+  }
+  return os;
 }
 
 std::ostream& operator<<(std::ostream& os, const ImageData& img) {
